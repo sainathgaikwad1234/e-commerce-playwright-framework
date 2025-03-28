c533a510447fc79a2dd8539cda18de64df14c828
sainathgaikwad1234 <sainath.gaikwad@thinkitive.com>
2025-03-28 17:45:35 +0530

    changes

--- pages/sauce-demo/login-page.ts
+++ pages/sauce-demo/login-page.ts
@@ -1,32 +1,3 @@
-// // pages/sauce-demo/login-page.ts
-// import { Page, Locator } from "@playwright/test";
-// import { SauceDemoBasePage } from "./sauce-demo-base-page";
-
-// export class LoginPage extends SauceDemoBasePage {
-//   readonly usernameInput: Locator;
-//   readonly passwordInput: Locator;
-//   readonly loginButton: Locator;
-//   readonly errorMessage: Locator;
-
-//   constructor(page: Page) {
-//     super(page);
-//     this.usernameInput = page.locator('[data-test="username"]');
-//     this.passwordInput = page.locator('[data-test="password"]');
-//     this.loginButton = page.locator('[data-test="login-button"]');
-//     this.errorMessage = page.locator('[data-test="error"]');
-//   }
-
-//   async goto(): Promise<void> {
-//     await this.navigate("/");
-//   }
-
-//   async login(username: string, password: string): Promise<void> {
-//     await this.usernameInput.fill(username);
-//     await this.passwordInput.fill(password);
-//     await this.loginButton.click();
-//   }
-// }
-
 // pages/sauce-demo/login-page.ts
 import { Page, Locator, expect } from "@playwright/test";
 import { SauceDemoBasePage } from "./sauce-demo-base-page";
